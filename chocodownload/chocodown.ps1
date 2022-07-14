4e134c086a44c28d31784ff7be2fd45a6837ce7f
Tyler Moody <tyler@moody.london>
2022-07-14 11:44:30 +0100

    Update chocodown.ps1

--- chocodownload/chocodown.ps1
+++ chocodownload/chocodown.ps1
@@ -1,18 +1,46 @@
 #Require Package name, version, repo
 $package = Read-Host -Prompt 'Input your package'
-$version = Read-Host -Prompt 'Input your Version (Leave Blank if you want latest version)'
-$repo = Read-Host -Prompt 'Input repo (admin or user)'
-#Repo
-$RepoURL = "<REPALCE_ME>'"
-$LocalRepoApiKey = "<REPALCE_ME>'"
-#Storage
-$dfs = "<REPALCE_ME>'"
-$tempPath = "C:\temp\"
-#If version was not entered find newest version
-if ($version -eq $null)
-{
-Write-host "Looking for latest version of '$package'."
 $version = (choco list $Package --exact -r --source="'https://licensedpackages.chocolatey.org/api/v2/;https://community.chocolatey.org/api/v2/'").Split('|')[1]
+#Repo
+$RepoURL = "<REPALCE_ME>"
+$LocalRepoApiKey = "<REPALCE_ME>"
+$uriSlack = "<REPALCE_ME>"
+#Storage
+$dfs = "<REPALCE_ME>"
+$tempPath = "C:\temp\"
+
+#Check Repo
+$validAnswer = $false
+While(-not $validAnswer)
+{
+    $yn = Read-Host "`What repo would you like to push $package to? type 'a' for admin or 'u' for user:"
+    Switch($yn.ToLower())
+    {
+        "a" {$validAnswer = $true
+            $repo = "admin"
+        }
+        "u" {$validAnswer = $true
+             $repo = "user"
+        }
+        Default {Write-Host "Try entering 'a' for admin or 'u' for user."}
+    }
+}
+
+#Check Version is the one you want
+$validAnswer = $false
+While(-not $validAnswer)
+{
+    $yn = Read-Host "`Newest version is $version would you like to download this y or n:"
+    Switch($yn.ToLower())
+    {
+        "y" {$validAnswer = $true
+            $version = $version
+        }
+        "n" {$validAnswer = $true
+             $version = Read-Host -Prompt 'Enter the version you would like'
+        }
+        Default {Write-Host "Try entering 'y' for yes or 'n' for no."}
+    }
 }
 
 #Download Pacakge
@@ -24,6 +52,7 @@ choco download $package --no-progress --internalize --version=$version --ignored
 if ($LASTEXITCODE -eq 0)
     {
         Write-host "Copying package '$package' to '$repo' FileShare."
+        $version = $version
         $localrepo = $RepoURL + $Repo + "/"
         $fileshare = $dfs + $Repo
         $temploc = $temppath + "download\" + $package
@@ -38,7 +67,7 @@ if ($LASTEXITCODE -eq 0)
         Write-host "Package '$package' pushed to repo. Notifying Choco Bot"
         $body = ConvertTo-Json @{
             pretext = "$package has been Internalized :new:"
-            text = "$package has sucessfully been pushed to $repo Repo. Version: $Version"
+            text = "$package with Version: $Version has sucessfully been pushed to $repo Repo."
         }
         Invoke-RestMethod -uri $uriSlack -Method Post -body $body -ContentType 'application/json' -TimeOut 120 | Out-Null
 
@@ -56,7 +85,7 @@ if ($LASTEXITCODE -eq 0)
                             Invoke-RestMethod -uri $uriSlack -Method Post -body $body -ContentType 'application/json' -TimeOut 120 | Out-Null
                         }
 Get-ChildItem $tempPath -Include *.nupkg -Recurse| Remove-Item -ErrorAction SilentlyContinue
-$tempdown = $temppath + "download\"
+$tempdown = $temppath + "download\*"
 Get-ChildItem $tempdown -Recurse | Remove-Item -ErrorAction SilentlyContinue
 Get-ChildItem * -Include *.nupkg  -Recurse| Remove-Item -ErrorAction SilentlyContinue
 exit
